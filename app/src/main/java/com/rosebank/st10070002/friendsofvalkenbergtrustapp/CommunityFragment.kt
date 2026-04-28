b6daacd447187cd6ebb142c7c5888eb788fe788a
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2026-04-28 22:24:47 +0200

    Events removed, Resources populated

--- app/src/main/java/com/rosebank/st10070002/friendsofvalkenbergtrustapp/CommunityFragment.kt
+++ app/src/main/java/com/rosebank/st10070002/friendsofvalkenbergtrustapp/CommunityFragment.kt
@@ -1,5 +1,16 @@
 package com.rosebank.st10070002.friendsofvalkenbergtrustapp
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 
-class CommunityFragment : Fragment(R.layout.fragment_community)
+class CommunityFragment : Fragment(R.layout.fragment_community) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
